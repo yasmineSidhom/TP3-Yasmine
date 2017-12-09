1b9c3142b9bf216d58bf8ab424a8920ed077be50
yasmineSidhom <33939598+yasmineSidhom@users.noreply.github.com>
2017-12-09 20:49:57 +0100

    derniere version du projet, tests integres

---
+++ TP-master/TP.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP;
+using TP.Controllers;
+
+namespace TP.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void IndexTest()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+       
+    }
+
+
+}
---
+++ TP-master/TP.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP;
+using TP.Controllers;
+using TP.Models;
+
+namespace TP.Tests.Controllers
+{
+    [TestClass]
+    public class DetailsTest
+    {
+        [TestMethod]
+        public void DetailsViewTest()
+        {
+            var controller = new HomeController();
+            var result = controller.Details(3) as ViewResult;
+            var m = (Movie)result.ViewData.Model;
+            Assert.AreEqual("The Dreamers", m.Title);
+        }
+    }
+}
